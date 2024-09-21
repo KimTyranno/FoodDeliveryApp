74420f229ebc2297f269b8cd4dea9fc7087b1786
tyranno <eotjdgkdl@gmail.com>
2024-09-22 03:15:11 +0900

    eslint, prettier설정, settings.json에 codeActionOnSave: {source.organizeImports}도 설정해야함

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  extends: ['@react-native', 'standard'],
+  plugins: ['@stylistic/js'],
+  rules: {
+    semi: 0,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 0,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    '@stylistic/js/indent': ['error', 2],
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
-};
+  printWidth: 100,
+  semi: false,
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-organize-imports'],
+  importOrder: ['^@mui/(.*)$', '^@untitled-ui/(.*)$', '^react', '^src/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
