2b3222b3d79c0e764eb4fd48fdfaf9c9f2643737
tyranno <eotjdgkdl@gmail.com>
2024-10-05 17:57:40 +0900

    앱을 재실행해도 로그인 유지하도록 추가

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import axios, { AxiosError } from 'axios'
 import * as React from 'react'
 import { useEffect } from 'react'
+import { Alert } from 'react-native'
+import Config from 'react-native-config'
+import EncryptedStorage from 'react-native-encrypted-storage'
 import { Provider, useSelector } from 'react-redux'
 import useSocket from './src/hooks/useSocket'
 import Delivery from './src/pages/Delivery'
@@ -10,7 +14,8 @@ import Orders from './src/pages/Orders'
 import Settings from './src/pages/Settings'
 import SignIn from './src/pages/SignIn'
 import SignUp from './src/pages/SignUp'
-import store from './src/store'
+import userSlice from './src/slices/user'
+import store, { useAppDispatch } from './src/store'
 import { RootState } from './src/store/reducer'
 
 export type LoggedInParamList = {
@@ -31,6 +36,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 export function AppInner() {
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email)
   const [socket, disconnect] = useSocket()
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     const callback = (data: any) => {
@@ -57,6 +63,42 @@ export function AppInner() {
       disconnect()
     }
   }, [isLoggedIn, disconnect])
+
+  // 앱 실행 시 토큰 있으면 로그인하는 코드
+  useEffect(() => {
+    const getTokenAndRefresh = async () => {
+      try {
+        const token = await EncryptedStorage.getItem('refreshToken')
+        if (!token) {
+          return
+        }
+        const response = await axios.post(
+          `${Config.API_URL}/refreshToken`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        )
+        dispatch(
+          userSlice.actions.setUser({
+            name: response.data.data.name,
+            email: response.data.data.email,
+            accessToken: response.data.data.accessToken,
+          }),
+        )
+      } catch (error) {
+        console.error(error)
+        if (error instanceof AxiosError && error.response?.data.code === 'expired') {
+          Alert.alert('알림', '다시 로그인 해주세요.')
+        }
+      } finally {
+        // TODO: 스플래시 스크린 없애기
+      }
+    }
+    getTokenAndRefresh()
+  }, [dispatch])
 
   return (
     <NavigationContainer>
